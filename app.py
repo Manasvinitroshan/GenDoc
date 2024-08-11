88539659bca3233c222b3e0580962ae45f16e3d3
Manasvinitroshan <manas.comet.25@gmail.com>
2024-08-11 00:07:17 -0500

    safety settings

--- app.py
+++ app.py
@@ -2,6 +2,32 @@ import streamlit as st
 from pathlib import Path
 import google.generativeai as genai
 
+from api_key import api_key
+
+genai.configure(api_key=api_key)
+
+# Create the model
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+
+#apply safety settings
+safety_settings[
+    
+    {
+        
+        
+
+}
+
+
+]
+
 #set page config
 
 st.set_page_config(page_title="VitalImage Analytics",page_icon=':robot')
